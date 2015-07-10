4a9392dc72e7132e9c0d6b91431809a082db9072
Johan Henriksson <johanhenriksson@live.com>
2015-07-10 22:52:19 +0200

    More commodities

--- core/commodity.go
+++ core/commodity.go
@@ -43,13 +43,25 @@ var COPPER = &Commodity {
 var DIAMOND = &Commodity {
     Type: 4,
     Name: "Diamond",
-    UnitWeight: 40000,
+    UnitWeight: 10 * GRAM,
+}
+
+var COAL = &Commodity {
+    Type: 5,
+    Name: "Coal",
+    UnitWeight: 1 * TON,
 }
 
 var STEEL = &Commodity {
     Type: 10,
     Name: "Steel",
     UnitWeight: 100 * KG,
+}
+
+var WOOD = &Commodity {
+    Type: 11,
+    Name: "Wood",
+    UnitWeigh: 100 * KG,
 }
 
 var FOOD = &Commodity {
@@ -64,8 +76,32 @@ var COFFEE = &Commodity {
     UnitWeight: 1 * TON,
 }
 
-var WEAPONS = &Commodity {
-    Type: 1337,
-    Name: "GUNS",
+var LEMONS = &Commodity {
+    Type: 101,
+    Name: "Lemons",
+    UnitWeight: 1 * TON,
+}
+
+var BEER = &Commodity {
+    Type: 102,
+    Name: "Beer",
+    UnitWeight: 100 * KG,
+}
+
+var EXPLOSIVES = &Commodity {
+    Type: 300,
+    Name: "Explosives",
+    UnitWeight: 1 * TON,
+}
+
+var GARBAGE = &Commodity {
+    Type: 400,
+    Name: "Garbage",
+    UnitWeight: 1 * TON,
+}
+
+var MAIL = &Commodity {
+    Type: 500,
+    Name: "Mail",
     UnitWeight: 1 * TON,
 }
